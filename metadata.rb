064e77bdd795be4534a890c4dd6bc7a2d74234b3
JohnMorales <jmorales@gmail.com>
2014-05-01 19:22:20 -0400

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'nodejs'
+
+maintainer       'John Morales'
+maintainer_email 'john@tenfoursolutions.com'
+license          'All rights reserved'
+description      'Installs/Configures nodejs'
+long_description 'Installs/Configures nodejs'
+version          '0.1.0'
+
+depends "nodejs"
