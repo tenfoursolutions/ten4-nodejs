c5a21c26badf9b7ca079f4b520b5f4a39f35ee41
JohnMorales <jmorales@gmail.com>
2014-05-16 19:32:48 -0400

    adding more tests

--- files/default/test/default_test.rb
+++ files/default/test/default_test.rb
@@ -22,4 +22,14 @@ class TestingNodeJSInstallation < MiniTest::Chef::TestCase
   def test_project_is_checkedout
     assert(FileTest.directory? "#{node['ten4-nodejs'][:application_directory]}/.git")
   end
+  def test_service_installed
+    assert(FileTest.executable? "/etc/init.d/#{node['ten4-nodejs'][:app]}")
+  end
+  def test_monit_is_running 
+    assert_running service("monit")
+  end
+  def test_site_is_up
+    response = `curl -s http://localhost/status`
+    assert_equal "Hello there!", response
+  end
 end
